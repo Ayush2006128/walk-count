d2ae54dc44515ecfc32f8a0fcd653b97cf96b996
Ayush Muley <74146837+Ayush2006128@users.noreply.github.com>
2025-12-17 10:48:58 +0530

    Fix TopAppBarDefaults import error in GoalsActivity

--- app/src/main/java/muley/ayush/walkcount/GoalsActivity.kt
+++ app/src/main/java/muley/ayush/walkcount/GoalsActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
